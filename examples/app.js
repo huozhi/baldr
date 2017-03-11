ebdea04ed83b3c046bd1cd99d407915a9ab78307
huozhi <gilesliu12@gmail.com>
2017-03-11 14:56:57 +0800

    provide some examples

---
+++ examples/app.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import {render} from 'react-dom'
+import Select from 'baldr/Select'
+import Dropdown from 'baldr/Dropdown'
+import Button from 'baldr/Button'
+import Card from 'baldr/Card'
+import styles from './app.css'
+
+const Showcase = ({title, children}) => (
+  <div className={styles.demo}>
+    <div className={styles.title}>{title}</div>
+    {children}
+  </div>
+)
+
+const App = () => (
+  <div className={styles.app}>
+    <div className={styles.name}>Baldr Components</div>
+    <Showcase title="Select">
+      <Select
+        options={[
+          {label: '1124123', value: 12312},
+          {label: '2124123', value: 22312},
+          {label: '3124123', value: 32312},
+          {label: '4124123', value: 42312},
+        ]}
+        value={42312}
+        title="demo select"
+        onSelect={() => {}}
+      />
+    </Showcase>
+
+    <Showcase title="Dropdown">
+      <Dropdown menu={<div className={styles.dropdown}>dropped</div>}>
+        <Button plain>dropdown toggler</Button>
+      </Dropdown>
+    </Showcase>
+
+    <Showcase title="Card">
+      <Card>
+        <Card.Header>Title</Card.Header>
+        <Card.Block>
+          Something Important
+        </Card.Block>
+      </Card>
+    </Showcase>
+
+  </div>
+)
+
+render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ examples/webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const joinPath = path.join.bind(null, __dirname)
+const isProduction = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  entry: {
+    app: [joinPath('app.js')],
+  },
+  output: {
+    publicPath: isProduction ? '/baldr' : '/',
+    path: joinPath('dist'),
+    filename: isProduction ? 'app.[hash].js' : 'app.js',
+  },
+  devtool: !isProduction && 'cheap-eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /examples\/.*\.css$/,
+        use: [
+          'style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[name]-[local]-[hash:base64:4]',
+          'postcss-loader'
+        ],
+      },
+      {
+        test: /src\/.*\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader'
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: ['babel-loader?presets[]=es2015,presets[]=react', 'svg-react-loader'],
+      }
+    ],
+  },
+  resolve: {
+    alias: {
+      'baldr': joinPath('../src'),
+    },
+  },
+  plugins: ([
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [
+          require('postcss-import')({addDependencyTo: webpack}),
+          require('postcss-nested'),
+          require('autoprefixer'),
+        ],
+      }
+    }),
+  ]).concat(isProduction ? [
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {warnings: false},
+      output: {comments: false},
+    }),
+    new HtmlWebpackPlugin({
+      template: joinPath('index-tpl.html'),
+      minify: false,
+    }),
+  ] : [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ]),
+  devServer: {
+    hot: true,
+    inline: true,
+    stats: {
+      colors: true,
+      chunks: false,
+      chunkModules: false,
+    },
+  },
+}
